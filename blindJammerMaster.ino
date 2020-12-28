0bfe42f51ecb9fae4217e57dafc9acf53853e1ef
eschmitty <30362930+eschmitty@users.noreply.github.com>
2020-12-28 11:06:34 -0500

    Switching RTC module to DS3234

--- blindJammerMaster.ino
+++ blindJammerMaster.ino
@@ -1,52 +1,62 @@
-//AutoWand Master v0.3
+//AutoWand Master v1.0
 //Evan Schmitt
 //2020
 
-#include <Wire.h>
-#include <RTClib.h>
+#include <SPI.h>
+#include <SparkFunDS3234RTC.h>
 #include <Dusk2Dawn.h>
 
 #define HWSERIAL Serial1
-#define sw1Pin 16;
-#define sw2Pin 17;
+#define sw1Pin 16
+#define sw2Pin 17
+#define DS13074_CS_PIN 10 // DeadOn RTC Chip-select pin
+
 
 int switch1State;
 int switch2State;
-
-RTC_PCF8523 rtc;
 
 void setup () {
 
   pinMode(sw1Pin, INPUT_PULLUP);
   pinMode(sw2Pin, INPUT_PULLUP);
-  
+
   Serial.begin(57600);
   HWSERIAL.begin(9600);
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
-  Dusk2Dawn belford(40.408146, -74.091941, -5);
 
-  // This line sets the RTC with an explicit date & time
-  // rtc.adjust(DateTime(2020, 11, 20, 0, 28, 0));
-  // following line sets the RTC to the date & time this sketch was compiled
-  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // Call rtc.begin([cs]) to initialize the library
+  // The chip-select pin should be sent as the only parameter
+  rtc.begin(DS13074_CS_PIN);
+  //rtc.set12Hour(); // Use rtc.set12Hour to set to 12-hour mode
+
+  // Now set the time...
+  // You can use the autoTime() function to set the RTC's clock and
+  // date to the compiler's predefined time. (It'll be a few seconds
+  // behind, but close!)
+  // rtc.autoTime();
+  // Or you can use the rtc.setTime(s, m, h, day, date, month, year)
+  // function to explicitly set the time:
+  // e.g. 7:32:16 | Monday October 31, 2016:
+  rtc.setTime(5, 53, 19, 1, 27, 12, 20);  // Uncomment to manually set time
+
+  rtc.update(); // Update time/date values
 }
 
 
 void loop () {
-  DateTime now = rtc.now();
-  int sunrise  = belford.sunrise(now.year(), now.month(), now.day(), false);
-  int sunset   = belford.sunset(now.year(), now.month(), now.day(), false);
-  switchState = digitalRead(swPin);
+  rtc.update();  // Update time/date values
+  Dusk2Dawn belford(40.408146, -74.091941, -5);
+  int sunrise  = belford.sunrise(rtc.year(), rtc.month(), rtc.date(), false);
+  int sunset   = belford.sunset(rtc.year(), rtc.month(), rtc.date(), false);
+  switch1State = digitalRead(sw1Pin);
+  switch2State = digitalRead(sw2Pin);
 
-  if ((now.hour() * 60) + now.minute() == sunrise - 5)
+  if ((rtc.hour() * 60) + rtc.minute() == sunrise - 5)
   {
     HWSERIAL.write("1");
     delay(15000);
   }
-  else if (((now.hour() * 60) + now.minute()) == sunset + 15)
+  else if (((rtc.hour() * 60) + rtc.minute()) == sunset + 25
+          )
   {
     HWSERIAL.write("2");
     delay(15000);
@@ -56,24 +66,25 @@ void loop () {
     HWSERIAL.write("3");
     delay(200);
   }
-  else if (switch2state == LOW)
+  else if (switch2State == LOW)
   {
     HWSERIAL.write("4");
     delay(200);
   }
-  
-  //Serial.print(now.year(), DEC);
-  //Serial.print('/');
-  //Serial.print(now.month(), DEC);
-  //Serial.print('/');
-  //Serial.print(now.day(), DEC);
-  //Serial.print(", ");
-  //Serial.print(now.hour(), DEC);
-  //Serial.print(':');
-  //Serial.print(now.minute(), DEC);
-  //Serial.print(':');
-  //Serial.print(now.second(), DEC);
-  //Serial.println();
-  //Serial.println(sunrise);
+/*
+  Serial.print(rtc.year(), DEC);
+  Serial.print('/');
+  Serial.print(rtc.month(), DEC);
+  Serial.print('/');
+  Serial.print(rtc.date(), DEC);
+  Serial.print(", ");
+  Serial.print(rtc.hour(), DEC);
+  Serial.print(':');
+  Serial.print(rtc.minute(), DEC);
+  Serial.print(':');
+  Serial.print(rtc.second(), DEC);
+  Serial.println();
+  Serial.println(sunrise);
+  */
   delay(100);
 }
